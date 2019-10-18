f712a676eb22b0114831ce6e6b15fe3a415955b4
PyARKio <fedoretss@gmail.com>
2019-10-18 18:18:33 +0300

    _

---
+++ drivers/get_socket.py
@@ -0,0 +1,119 @@
+# -- coding: utf-8 --
+from __future__ import unicode_literals
+import threading
+import socket
+from time import sleep
+import time
+from multiprocessing import Queue
+from drivers.log_settings import log
+
+
+__author__ = "PyARKio"
+__version__ = "1.0.1"
+__email__ = "fedoretss@gmail.com"
+__status__ = "Production"
+
+
+class CommonQueue:
+    CQ = Queue()
+    SysCQ = Queue()
+    SCQ = Queue()
+
+
+def _init_client(f_object=None):
+    log.info(f_object)
+
+    def _f(*object_to_check):
+        log.info('init client...')
+        log.info(f_object)
+        log.info(object_to_check)
+        try:
+            _socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # , 'i')
+        except Exception as err:
+            log.error(err)
+            CommonQueue.SCQ.put(False)
+            return False
+        else:
+            _socket.setblocking(False)
+            _socket.settimeout(1)
+        log.info('function: <{}>, host: {}, STATUS: OK'.format(f_object.__name__, object_to_check[0]))
+        return f_object(*object_to_check, _socket)
+    return _f
+
+
+@_init_client
+def check_host(host=None, port=None, _socket=None):
+    log.info('Connecting to {}:{}:{} ...'.format(host, port, _socket))
+    try:
+        _socket.connect((host, port))
+    except Exception as err:
+        log.error('HOST: {} ERROR: {}'.format(host, err))
+        CommonQueue.SCQ.put(False)
+    else:
+        log.info('Connect :) HOST: {}'.format(host))
+        _server_identification(host, _socket)
+
+
+def _server_identification(host, _socket):
+    log.info('for host: {}'.format(host))
+    try:
+        data = _socket.recv(1024).decode()
+    except Exception as err:
+        log.info('HOST: {} ERROR: {}'.format(host, err))
+        CommonQueue.SCQ.put(False)
+    else:
+        log.info('response from server: {}'.format(data))
+        if data == 'check\r\n':
+            log.info('send to server: {}'.format(bytes('check ok', encoding='UTF-8')))
+            _socket.send(bytes('check ok', encoding='UTF-8'))
+            CommonQueue.SCQ.put(_socket)
+            # data = _socket.recv(1024).decode()
+            while True:
+                sleep(10)
+        else:
+            CommonQueue.SCQ.put(False)
+
+
+# PRE_HOST: is not correct method !!!
+# @CHECK_args !!!
+def search_host(pre_host=None, port=None, _socket_connect=None, auto=False, word=[None, None]):
+    _socket = False
+
+    if auto:
+        log.info('AUTO MODE')
+        point = time.time()
+        for f in range(0, 256, 4):
+            for i in range(f, f + 4):
+                host = '{}.{}'.format(pre_host, i)
+                assistant_start = threading.Thread(target=check_host, args=(host, port, ))
+                assistant_start.start()
+
+            index = 0
+            while index < 4:
+                if not _socket:
+                    _socket = CommonQueue.SCQ.get()
+                else:
+                    CommonQueue.SCQ.get()
+                index += 1
+
+            if _socket:
+                log.info('BREAK')
+                log.info(time.time() - point)
+                log.info(_socket)
+                break
+
+        # for i in range(5):
+        #     log.info('send to server: {}'.format(bytes('BREAK :)', encoding='UTF-8')))
+        #     _socket.send(bytes('BREAK :)', encoding='UTF-8'))
+        #     sleep(5)
+        # data = _socket.recv(10000)
+        # _socket = None
+    else:
+        log.info('MANUAL MODE')
+        check_host(pre_host, port)
+        _socket = CommonQueue.SCQ.get()
+
+    return _socket
+
+
+# search_host(pre_host='10.8.0', port=777, auto=True)
--- tcp_client.py
+++ tcp_client.py
@@ -1,21 +1,11 @@
 # -- coding: utf-8 --
 from __future__ import unicode_literals
 import threading
-import socket
-from time import sleep
-import time
-from datetime import datetime
-import json
-import pickle
-# from queue import Queue
-# from collections import deque
 from multiprocessing import Queue
+from drivers import get_socket
 from drivers.log_settings import log
-from drivers.interrupt import Interrupt
-from helpers import Exceptions
-from helpers import decorators
-from helpers import checkers
-import types
+from time import sleep
+import socket
 
 
 __author__ = "PyARKio"
@@ -31,97 +21,74 @@ class CommonQueue:
 
 
 class Client(threading.Thread):
-    def __init__(self):
+    def __init__(self, host=None, port=None, auto=False, data_callback=None, system_callback=None):
         threading.Thread.__init__(self)
 
+        self.host = host
+        self.port = port
+        self.auto = auto
+        self.data_callback = data_callback
+        self.system_callback = system_callback
+        self.running = True
+        # self._socket_connect = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # self._socket_connect.setblocking(False)
+        # self._socket_connect.settimeout(1)
+        self._socket = False
+
     def run(self):
-        pass
+        while self.running:
+            # _socket = False
+            while not self._socket:
+                self._socket = get_socket.search_host(pre_host=self.host, port=self.port, auto=self.auto)  # , _socket_connect=self._socket_connect)
+                log.info(self._socket)
+                self.system_callback(self._socket)
+
+            # _socket.connect(('10.8.0.5', 777))
+            while self._socket:
+                log.info('\n\nREAD\n\n')
+                try:
+                    data = self._socket.recv(10000)
+                except Exception as err:
+                    log.info('ERROR: {}'.format(err))
+                    # self._socket = None
+                else:
+                    self.data_callback(data.decode('cp1251'))
+
+                # for i in range(5):
+                # log.info('send to server: {}'.format(bytes('BREAK :)', encoding='UTF-8')))
+                # self._socket.send(bytes('BREAK :)', encoding='UTF-8'))
+                #     sleep(5)
+
+                # data = _socket.recv(10000)
+                # self._socket = None
 
 
-def _init_client(f_object=None):
-    log.info(f_object)
+class Sender(threading.Thread):
+    def __init__(self, sender_socket=None):
+        threading.Thread.__init__(self)
+        self.running = True
+        self.sender_socket = sender_socket
 
-    def _f(*object_to_check):
-        log.info('init client...')
-        log.info(f_object)
-        log.info(object_to_check)
-        try:
-            _socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # , 'i')
-        except Exception as err:
-            log.error(err)
-            return False
-        else:
-            _socket.setblocking(False)
-            _socket.settimeout(1)
-        log.info('function: <{}>, STATUS: OK'.format(f_object.__name__))
-        return f_object(*object_to_check, _socket)
-    return _f
+    def run(self):
+        while self.running:
+            data = CommonQueue.CQ.get()
+            # check data type
+            self.sender_socket.send(bytes(data, encoding='UTF-8'))
 
 
-@_init_client
-def check_host(host=None, port=None, _socket=None):
-    log.info('Connecting to {}:{}:{} ...'.format(host, port, _socket))
-    try:
-        _socket.connect((host, port))
-    except Exception as err:
-        log.info('HOST: {} ERROR: {}'.format(host, err))
-        CommonQueue.SCQ.put(False)
-    else:
-        log.info('Connect :) HOST: {}'.format(host))
-        _server_identification(host, _socket)
+def callback_data(response):
+    log.info(response)
 
 
-def _server_identification(host, _socket):
-    log.info('')
-    try:
-        data = _socket.recv(1024).decode()
-    except Exception as err:
-        log.info('HOST: {} ERROR: {}'.format(host, err))
-        CommonQueue.SCQ.put(False)
-    else:
-        log.info('response from server: {}'.format(data))
-        if data == 'check\r\n':
-            log.info('send to server: {}'.format(bytes('check ok', encoding='UTF-8')))
-            _socket.send(bytes('check ok', encoding='UTF-8'))
-            CommonQueue.SCQ.put(_socket)
-        else:
-            CommonQueue.SCQ.put(False)
+def callback_system(response):
+    log.info(response)
 
 
-# check_host('10.8.0.5', 777)
+_client = Client(host='10.8.0.5', port=777, auto=False, data_callback=callback_data, system_callback=callback_system)
+_client.start()
 
 
-def auto_search_host(pre_host, port):
-    point = time.time()
-    _socket = False
-    for f in range(0, 256, 4):
-        for i in range(f, f + 4):
-            host = '{}.{}'.format(pre_host, i)
-            # host = '192.168.1.{}'.format(i)
-            assistant_start = threading.Thread(target=check_host, args=(host, port, ))
-            assistant_start.start()
-
-        index = 0
-        while index < 4:
-            if not _socket:
-                _socket = CommonQueue.SCQ.get()
-            else:
-                CommonQueue.SCQ.get()
-            index += 1
-
-        if _socket:
-            log.info('BREAK')
-            break
-
-        log.info(time.time() - point)
-        log.info(_socket)
-    for i in range(5):
-        log.info('send to server: {}'.format(bytes('BREAK :)', encoding='UTF-8')))
-        _socket.send(bytes('BREAK :)', encoding='UTF-8'))
-        sleep(5)
-    _socket = None
-
-
-auto_search_host('10.8.0', 777)
+while True:
+    pass
 
 
