31061b69b9a887d8e5e9f05f4caf01ca432c3ba6
PyARKio <fedoretss@gmail.com>
2019-10-25 01:10:40 +0300

    detection lost

--- tcp_client.py
+++ tcp_client.py
@@ -5,6 +5,10 @@ from multiprocessing import Queue
 from drivers import get_socket
 from drivers.log_settings import log
 from time import sleep
+import sys
+import socket
+
+print(sys.version)
 
 
 __author__ = "PyARKio"
@@ -33,35 +37,56 @@ class Client(threading.Thread):
 
     def run(self):
         while self.running:
-            _socket = False
+            _sender = None
+            _socket = None
             log.info('host circle. _socket: {}'.format(_socket))
             while not _socket:
-                log.info('init search host')
-                _socket = get_socket.search_host(pre_host=self.host, port=self.port, auto=self.auto)
-                log.info(_socket)
-                self.system_callback(_socket)
-
+                _socket = self._get_socket()
+            if _socket:
+                _sender = self._run_sender(_socket)
             while _socket:
-                _sender = Sender(sender_socket=_socket)
-                _sender.start()
+                _socket = self._get_data_from_server(_socket, _sender)
 
-                log.info('\n\nREADY to read\n\n')
-                _socket.setblocking(True)
-                try:
-                    data = _socket.recv(10000)
-                except Exception as err:
-                    log.info('ERROR: {}'.format(err))
-                    _socket = False
-                    self.system_callback({'ERROR': '{}'.format(err)})
-                else:
-                    self.data_callback(data.decode('cp1251'))
-                    # _socket.setblocking(False)
+    def _get_socket(self):
+        log.info('init search host')
+        _socket = get_socket.search_host(pre_host=self.host, port=self.port, auto=self.auto)
+        log.info(_socket)
+        self.system_callback(_socket)
 
-                # for i in range(5):
-                # log.info('send to server: {}'.format(bytes('BREAK :)', encoding='UTF-8')))
-                # _socket.send(bytes('BREAK :)', encoding='UTF-8'))
-                #     sleep(5)
-                # _socket = None
+        return _socket
+
+    @staticmethod
+    def _run_sender(_socket):
+        _sender = Sender(sender_socket=_socket)
+        _sender.start()
+        _socket.settimeout(1)
+
+        return _sender
+
+    def _get_data_from_server(self, _socket, _sender):
+        log.info('\n\nREADY to read\n\n')
+        try:
+            data = _socket.recv(1024).decode()
+        except socket.timeout as err:
+            pass
+            log.error('GOOD ERROR: {}'.format(err))
+        except Exception as err:
+            _socket = False
+            self._socket_error(err, _sender)
+        else:
+            if not data:
+                log.error('No data from {}'.format(data))
+            else:
+                self.data_callback(data)  # .decode('cp1251')
+        return _socket
+
+    def _socket_error(self, err, _sender):
+        log.info('ERROR: {}'.format(err))
+        # STOP SENDER
+        _sender.running = False
+        CommonQueue.CQ.put(False)
+        # SEND TO PARENT
+        self.system_callback({'ERROR': '{}'.format(err)})
 
 
 class Sender(threading.Thread):
@@ -75,15 +100,19 @@ class Sender(threading.Thread):
         threading.Thread.__init__(self)
 
     def run(self):
-        log.info('running...')
+        log.info('SENDER running...')
         while self.running:
             response = Sender.__get_from_queue()
             if response:
                 # check data type
-                self.sender_socket.send(bytes(response, encoding='UTF-8'))
+                try:
+                    self.sender_socket.send(bytes(response, encoding='UTF-8'))
+                except Exception as err:
+                    log.error('SENDER: {}'.format(err))
+                    # self.system_callback()
             else:
-                self.system_callback()
-
+                log.info('SENDER: response = {}'.format(response))
+                # self.system_callback()
 
     @staticmethod
     def __get_from_queue():
@@ -102,14 +131,17 @@ def callback_data(response):
 
 def callback_system(response):
     log.info(response)
+    log.info('START WRITE TO LOCAL DB :)')
 
 
 if __name__ == '__main__':
-    # host='10.8.0'
-    _client = Client(host='192.168.1', port=777, auto=True, data_callback=callback_data, system_callback=callback_system)
+    _client = Client(host='10.8.0', port=777, auto=True, data_callback=callback_data, system_callback=callback_system)
     _client.start()
 
     while True:
-        sleep(10)
+        for i in range(500):
+            log.info('send to server: {}'.format(bytes('BREAK :)', encoding='UTF-8')))
+            CommonQueue.CQ.put('BREAK :)')
+            sleep(5)
 
 
